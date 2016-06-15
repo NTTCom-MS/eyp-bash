926ea91c2fa8a193ebd3dbff5cf684821db27080
Jordi Prats <jprats@systemadmin.es>
2016-06-15 18:57:29 +0200

    suport sles11sp3

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,6 +10,24 @@ class bash::params {
     {
       $debconf='debconf-utils'
     }
+    'Suse':
+    {
+      case $::operatingsystem
+      {
+        'SLES':
+        {
+          case $::operatingsystemrelease
+          {
+            '11.3':
+            {
+              $debconf=undef
+            }
+            default: { fail("Unsupported operating system ${::operatingsystem} ${::operatingsystemrelease}") }
+          }
+        }
+        default: { fail("Unsupported operating system ${::operatingsystem}") }
+      }
+    }
     default: { fail('Unsupported OS!')  }
   }
 
