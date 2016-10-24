ce8fb988b7e106c999c58c1e0f241f6e5d33ae8b
Jordi Prats <jprats@systemadmin.es>
2016-10-24 12:14:38 +0200

    environments

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # CHANGELOG
 
+## 0.1.8
+
+* added **bash::environment** to be able to add environment variables
+
 ## 0.1.7
 
 * added **bash::prompt** to be able to change user's prompt
---
+++ templates/environment.pp
@@ -0,0 +1,8 @@
+<% if defined?(@user) -%>
+if [ "$(id -un)" == "<%= @user %>" ];
+then
+<% end -%>
+<% if defined?(@user) %>  <% end %>export <%= @envname %>="<%= @value %>"
+<% if defined?(@user) -%>
+fi
+<% end -%>
